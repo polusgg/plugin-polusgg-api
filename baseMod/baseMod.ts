b4d3542fa52c88607e014582d94f1f3376919ec3
Rose Hall <rose@hall.ly>
2021-03-27 21:34:22 -0400

    remove example mod

--- baseMod/baseMod.ts
+++ baseMod/baseMod.ts
@@ -1,8 +1,6 @@
 import { RoleAssignmentData } from "../services/roleManager/roleManagerService";
 import { PluginMetadata } from "@nodepolus/framework/src/api/plugin";
 import { LobbyInstance } from "@nodepolus/framework/src/api/lobby";
-import { TextComponent } from "@nodepolus/framework/src/api/text";
-import { BaseRole } from "../baseRole";
 
 export class BaseMod {
   constructor(
@@ -11,31 +9,4 @@ export class BaseMod {
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   getRoles(lobby: LobbyInstance): RoleAssignmentData[] { return [] }
-}
-
-/// example mod
-
-export class Jester extends BaseRole {
-
-}
-
-export default class extends BaseMod {
-  constructor() {
-    super({
-      name: "JesterMod",
-      version: [1, 0, 0],
-    });
-  }
-
-  getRoles(_lobby: LobbyInstance): RoleAssignmentData[] {
-    return [{
-      playerCount: 2,
-      role: Jester,
-      startGameScreen: {
-        title: new TextComponent().setColor(215, 72, 154).add("Jester"),
-        subtitle: new TextComponent().add("You are the Jester. Get voted out to win!"),
-        color: [215, 72, 154, 255],
-      },
-    }];
-  }
 }

af5f8b746bb6d6ea2f15b95a9677ff78500f88c2
Rose Hall <rose@hall.ly>
2021-04-10 18:04:05 -0400

    beginnings of attachables

--- src/entities/entityVent.ts
+++ src/entities/entityVent.ts
@@ -1,8 +1,11 @@
 import { LobbyInstance } from "@nodepolus/framework/src/api/lobby";
 import { BaseInnerNetEntity } from "@nodepolus/framework/src/protocol/entities/baseEntity";
+import { Vector2 } from "@nodepolus/framework/src/types";
 import { SpawnFlag } from "@nodepolus/framework/src/types/enums";
 import { GLOBAL_OWNER } from "@nodepolus/framework/src/util/constants";
+import { InnerCustomNetworkTransformGeneric } from "../innerNetObjects/innerCustomNetworkTransformGeneric";
 import { InnerVent } from "../innerNetObjects/innerVent";
+import { EdgeAlignments } from "../types/enums/edgeAlignment";
 
 export class EntityVent extends BaseInnerNetEntity {
   constructor(
@@ -14,17 +17,23 @@ export class EntityVent extends BaseInnerNetEntity {
     ventSpriteResourceId: number,
     enterVentAnimationResourceId: number,
     exitVentAnimationResourceId: number,
+    position: Vector2 = Vector2.zero(),
     ventNetId: number = lobby.getHostInstance().getNextNetId(),
   ) {
     super(0x84, lobby, GLOBAL_OWNER, SpawnFlag.None);
 
     this.innerNetObjects = [
       new InnerVent(this, ventNetId, ventSpriteResourceId, enterVentAnimationResourceId, exitVentAnimationResourceId, ventId, leftConnection, rightConnection, centerConnection),
+      new InnerCustomNetworkTransformGeneric(this, EdgeAlignments.None, position),
     ];
   }
 
   getVent(): InnerVent {
     return this.getObject(0);
+  }
+
+  getCustomNetworkTransform(): InnerCustomNetworkTransformGeneric {
+    return this.getObject(1);
   }
 
   despawn(): void {
--- src/innerNetObjects/innerCustomNetworkTransformGeneric.ts
+++ src/innerNetObjects/innerCustomNetworkTransformGeneric.ts
@@ -1,18 +1,22 @@
 import { BaseInnerNetEntity, BaseInnerNetObject } from "@nodepolus/framework/src/protocol/entities/baseEntity";
-import { InnerNetObjectType, RpcPacketType } from "@nodepolus/framework/src/types/enums";
 import { DataPacket, SpawnPacketObject } from "@nodepolus/framework/src/protocol/packets/gameData";
-import { BaseRpcPacket } from "@nodepolus/framework/src/protocol/packets/rpc";
+import { InnerCustomNetworkTransform } from "@nodepolus/framework/src/protocol/entities/player";
 import { MessageReader, MessageWriter } from "@nodepolus/framework/src/util/hazelMessage";
+import { InnerNetObjectType, RpcPacketType } from "@nodepolus/framework/src/types/enums";
+import { BaseRpcPacket } from "@nodepolus/framework/src/protocol/packets/rpc";
 import { Connection } from "@nodepolus/framework/src/protocol/connection";
-import { Vector2 } from "@nodepolus/framework/src/types";
-import { EdgeAlignments } from "../types/enums/edgeAlignment";
 import { SnapToPacket } from "../packets/rpc/customNetworkTransform";
+import { EdgeAlignments } from "../types/enums/edgeAlignment";
+import { Vector2 } from "@nodepolus/framework/src/types";
+import { Player } from "@nodepolus/framework/src/player";
+import { Attachable } from "../types/attachable";
 
 export class InnerCustomNetworkTransformGeneric extends BaseInnerNetObject {
   constructor(
     protected readonly parent: BaseInnerNetEntity,
     protected alignment: EdgeAlignments = EdgeAlignments.None,
     protected position: Vector2 = Vector2.zero(),
+    protected attachedTo: number = -1,
     netId: number = parent.getLobby().getHostInstance().getNextNetId(),
   ) {
     super(InnerNetObjectType.CustomNetworkTransform, parent, netId);
@@ -70,6 +74,7 @@ export class InnerCustomNetworkTransformGeneric extends BaseInnerNetObject {
     const reader = MessageReader.fromRawBytes(packet.getBuffer());
 
     this.position = reader.readVector2();
+    this.attachedTo = reader.readPackedInt32();
   }
 
   serializeSpawn(): SpawnPacketObject {
@@ -77,11 +82,32 @@ export class InnerCustomNetworkTransformGeneric extends BaseInnerNetObject {
 
     return new SpawnPacketObject(
       this.netId,
-      writer.writeVector2(this.position),
+      writer.writeVector2(this.position).writePackedInt32(this.attachedTo),
     );
   }
 
   clone(): InnerCustomNetworkTransformGeneric {
     return new InnerCustomNetworkTransformGeneric(this.parent, this.alignment, this.position, this.netId);
+  }
+
+  setAttachedTo(object: Attachable): void {
+    this.attachedTo = this.findOwner(object);
+  }
+
+  protected findOwner(object: Attachable): number {
+    if (object instanceof Player) {
+      return object.getEntity().getCustomNetworkTransform().getNetId();
+    }
+
+    if (object instanceof BaseInnerNetEntity) {
+      return object.getCustomNetworkTransform().getNetId();
+    }
+
+    if (object instanceof InnerCustomNetworkTransform || object instanceof InnerCustomNetworkTransformGeneric) {
+      return object.getNetId();
+    }
+
+    console.log(object);
+    throw new Error("Could not find owner of object");
   }
 }

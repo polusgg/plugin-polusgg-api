a45888b8da629a119ffee00e2d7c9a84dcf46b0d
cybershard <ashwatdd01@gmail.com>
2021-04-16 12:25:20 -0400

    Update SetString Locations to the latest spec

--- src/types/enums/location.ts
+++ src/types/enums/location.ts
@@ -1,4 +1,9 @@
 export enum Location {
-  GameCode,
-  GamePlayerCount,
+  GameCode,        // e.g. Code\nXADFJK
+  GamePlayerCount, // e.g. 8 / 10
+  TaskText,
+  RoomTracker,
+  PingTracker,     // NOTE: setting this prevents the ping from updating until it's set to the string "__unset" at which point it will go back to default behaviour. Strings can also be sent with %s, like "Your ping is: %s" which would update live w/ your ping
+  TaskCompletion,  // e.g. "TOTAL TASKS COMPLETED"
+  GameOptions
 }

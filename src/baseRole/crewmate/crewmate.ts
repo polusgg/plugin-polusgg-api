fcf026a04c063f97b9dec83edb51fe03f8d5180c
Sanae <atmatm6000@outlook.com>
2021-04-24 18:15:01 -0600

    Fix missing > in crewmate assignment screen

--- src/baseRole/crewmate/crewmate.ts
+++ src/baseRole/crewmate/crewmate.ts
@@ -18,7 +18,7 @@ export class Crewmate extends BaseRole {
   getAssignmentScreen(_player: PlayerInstance): StartGameScreenData {
     return {
       title: `Crewmate`,
-      subtitle: `There is <color=#${Palette.impostorRed().map(e => e.toString(16)).join("")}>Impostors</color among us`,
+      subtitle: `There is <color=#${Palette.impostorRed().map(e => e.toString(16)).join("")}>Impostors</color> among us`,
       color: Palette.crewmateBlue(),
     };
   }

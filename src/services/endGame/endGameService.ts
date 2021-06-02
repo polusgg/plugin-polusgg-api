9a3f61a214f569000eee9125b16e902cf4a73fdd
Rose Hall <rose@hall.ly>
2021-06-02 18:51:48 -0400

    added endGameIntents and endGameExclusions

--- src/services/endGame/endGameService.ts
+++ src/services/endGame/endGameService.ts
@@ -1,7 +1,17 @@
 import { Game } from "@nodepolus/framework/src/api/game";
+import { Player } from "@nodepolus/framework/src/player";
 import { Connection } from "@nodepolus/framework/src/protocol/connection";
 import { OverwriteGameOver } from "../../packets/root";
 import { EndGameScreenData } from "../roleManager/roleManagerService";
+
+type EndGameIntent = {
+  endGameData: Map<Player, EndGameScreenData>;
+  intentName: string;
+};
+
+type EndGameExclusion = {
+  intentName: string;
+};
 
 export class EndGameService {
   //@TODO: Setters and getters for defaultEndGameData
@@ -13,6 +23,9 @@ export class EndGameService {
     displayQuit: true,
     displayPlayAgain: true,
   };
+
+  protected intents: Map<Game, EndGameIntent[]> = new Map();
+  protected exclusions: Map<Game, EndGameExclusion[]> = new Map();
 
   async setEndGameData(connection: Connection | undefined, endGameData: EndGameScreenData): Promise<void> {
     connection?.setMeta("pgg.api.endGameData", endGameData);
@@ -37,5 +50,45 @@ export class EndGameService {
     });
 
     game.getLobby().getHostInstance().endGame(0x07);
+  }
+
+  registerEndGameIntent(game: Game, endGameIntent: EndGameIntent): void {
+    if (!this.intents.has(game)) {
+      this.intents.set(game, []);
+    }
+
+    if (!this.intents.get(game)!.some(item => item.intentName === endGameIntent.intentName)) {
+      this.intents.get(game)!.push(endGameIntent);
+    }
+
+    this.recalculateEndGame(game);
+  }
+
+  registerExclusion(game: Game, exclusion: EndGameExclusion): void {
+    if (!this.exclusions.has(game)) {
+      this.exclusions.set(game, []);
+    }
+
+    if (!this.exclusions.get(game)!.some(item => item.intentName === exclusion.intentName)) {
+      this.exclusions.get(game)!.push(exclusion);
+    }
+
+    this.recalculateEndGame(game);
+  }
+
+  recalculateEndGame(game: Game): void {
+    for (let i = 0; i < this.intents.get(game)!.length; i++) {
+      const intent = this.intents.get(game)![i];
+
+      if (!this.exclusions.get(game)!.some(exclusion => exclusion.intentName === intent.intentName)) {
+        [...intent.endGameData.entries()].forEach(([player, data]) => {
+          if (player.getConnection() !== undefined) {
+            this.setEndGameData(player.getConnection(), data);
+          }
+        });
+
+        this.endGame(game);
+      }
+    }
   }
 }

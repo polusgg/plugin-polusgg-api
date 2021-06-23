ab716fab509d55486e78556a472cbc21f1f5d249
Jan Przebor <przebot@gmail.com>
2021-06-23 19:33:03 +0200

    Implement SetHudVisibility packet in HUD service

--- src/services/hud/hudService.ts
+++ src/services/hud/hudService.ts
@@ -3,7 +3,9 @@ import { Player } from "@nodepolus/framework/src/player";
 import { Server } from "@nodepolus/framework/src/server";
 import { SetStringPacket } from "../../packets/root";
 import { DisplaySystemAlertPacket } from "../../packets/root/displaySystemAlert";
+import { SetHudVisibilityPacket } from "../../packets/root/setHudVisibilityPacket";
 import { Location } from "../../types/enums";
+import { HudItem } from "../../types/enums/hudItem";
 
 declare const server: Server;
 
@@ -14,5 +16,9 @@ export class HudService {
 
   async setHudString(player: PlayerInstance, location: Location, text: string): Promise<void> {
     await (player as Player).getConnection()?.writeReliable(new SetStringPacket(text, location));
+  }
+
+  async setHudVisibility(player: PlayerInstance, item: HudItem, enabled: boolean): Promise<void> {
+    await (player as Player).getConnection()?.writeReliable(new SetHudVisibilityPacket(item, enabled));
   }
 }

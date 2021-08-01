908f9ee848f7e17220793c29338fc7016b6e3f54
Dumb Player2 <dumbplayer2@polus.gg>
2021-08-01 22:27:22 +0200

    Capitalization Fix

--- src/baseRole/impostor/impostor.ts
+++ src/baseRole/impostor/impostor.ts
@@ -225,7 +225,7 @@ Fake Tasks:`;
   getAssignmentScreen(_player: PlayerInstance, _impostorCount: number): StartGameScreenData {
     return {
       title: `Impostor`,
-      subtitle: `Kill the <color=#8CFFFFFF>crewmates</color>`,
+      subtitle: `Kill the <color=#8CFFFFFF>Crewmates</color>`,
       color: Palette.impostorRed(),
     };
   }
